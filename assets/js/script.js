bca3a4feb3b41a604d4d200ec4462f314738fc1d
hedgemaster <hedgemaster29@gmail.com>
2021-03-24 15:26:31 +0800

    First page(SP)

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,46 @@ jQuery(function($){
             $(this).addClass("active");
             $("#"+$(this).data("tab")).parent().children(".tab-content").hide();
             $("#"+$(this).data("tab")).show();
+        });
+
+        if($(this).scrollTop() > 1000) {
+            $('#to-top').fadeIn();
+        } else {
+            $('#to-top').fadeOut();
+        }
+
+        $(window).scroll(function(){
+            if($(this).scrollTop() > 1000) {
+                $('#to-top').fadeIn();
+            } else {
+                $('#to-top').fadeOut();
+            }
+        });
+
+        $("#to-top").click(function () {
+            $("html, body").animate({scrollTop: 0}, 300);
+        });
+
+        $(".nav-burger").off("click").on("click", function(){
+            if($("body").hasClass("nav-open")) {
+                $("body").removeClass("nav-open");
+            } else {
+                $("body").addClass("nav-open");
+            }
+        });
+
+        $(".overlay").off("click").on("click", function(){
+            $("body").removeClass("nav-open");
+        });
+
+        $(".subnav-opener").off("click").on("click", function(){
+            if($(this).parent().hasClass("active")) {
+                $(this).parent().removeClass("active");
+                $(this).parent().children("ul").slideUp();
+            } else {
+                $(this).parent().addClass("active");
+                $(this).parent().children("ul").slideDown();
+            }
         })
     });
 });
