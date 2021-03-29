629f411ca2ba80ad7c8113dac5814acabdbe321d
CleanLake <three_star123@hotmail.com>
2021-03-29 22:14:09 +0900

    担当からの結果物 (2021.3.29)

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,50 +87,52 @@ jQuery(function($){
             });
         }
 
-        var tab_detail_pos = $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").offset().top;
-        var side_tab_top = 0;
-
-        $(window).scroll(function(){
-            if($(window).width()>=768) {
-                tab_detail_pos = $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").offset().top;
-
-                $("body.page-news .sec-news .sec-row .sec-right > ul.news-tabs.custom-tab").each(function(){
-                    if(($(window).scrollTop() > tab_detail_pos - 1) && (tab_detail_pos + $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").outerHeight()) > ($(window).scrollTop() + $(this).outerHeight() + 1)) {
-                        side_tab_top = $(window).scrollTop() - tab_detail_pos + 1;
-                        $(this).css('top',side_tab_top + 'px');
-                    } else if ($(window).scrollTop() < tab_detail_pos || $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").outerHeight() < $(this).outerHeight()) {
-                        side_tab_top = 0;
-                        $(this).css('top',side_tab_top + 'px');
-                    } else {
-                        side_tab_top = $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").outerHeight() - $(this).outerHeight();
-                        $(this).css('top',side_tab_top + 'px');
-                    }
-                });
-            } else {
-                $("body.page-news .sec-news .sec-row .sec-right > ul.news-tabs.custom-tab").css("top","0");
-            }
-        });
-
-        $(window).resize(function(){
+        if($("body.page-news .sec-news .sec-row .sec-left > ul.news-list").length) {
             var tab_detail_pos = $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").offset().top;
-            if($(window).width()>=768) {
-                tab_detail_pos = $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").offset().top;
+            var side_tab_top = 0;
 
-                $("body.page-news .sec-news .sec-row .sec-right > ul.news-tabs.custom-tab").each(function(){
-                    if(($(window).scrollTop() > tab_detail_pos - 1) && (tab_detail_pos + $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").outerHeight()) > ($(window).scrollTop() + $(this).outerHeight() + 1)) {
-                        side_tab_top = $(window).scrollTop() - tab_detail_pos + 1;
-                        $(this).css('top',side_tab_top + 'px');
-                    } else if ($(window).scrollTop() < tab_detail_pos || $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").outerHeight() < $(this).outerHeight()) {
-                        side_tab_top = 0;
-                        $(this).css('top',side_tab_top + 'px');
-                    } else {
-                        side_tab_top = $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").outerHeight() - $(this).outerHeight();
-                        $(this).css('top',side_tab_top + 'px');
-                    }
-                });
-            } else {
-                $("body.page-news .sec-news .sec-row .sec-right > ul.news-tabs.custom-tab").css("top","0");
-            }
-        });
+            $(window).scroll(function(){
+                if($(window).width()>=768) {
+                    tab_detail_pos = $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").offset().top;
+
+                    $("body.page-news .sec-news .sec-row .sec-right > ul.news-tabs.custom-tab").each(function(){
+                        if(($(window).scrollTop() > tab_detail_pos - 1) && (tab_detail_pos + $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").outerHeight()) > ($(window).scrollTop() + $(this).outerHeight() + 1)) {
+                            side_tab_top = $(window).scrollTop() - tab_detail_pos + 1;
+                            $(this).css('top',side_tab_top + 'px');
+                        } else if ($(window).scrollTop() < tab_detail_pos || $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").outerHeight() < $(this).outerHeight()) {
+                            side_tab_top = 0;
+                            $(this).css('top',side_tab_top + 'px');
+                        } else {
+                            side_tab_top = $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").outerHeight() - $(this).outerHeight();
+                            $(this).css('top',side_tab_top + 'px');
+                        }
+                    });
+                } else {
+                    $("body.page-news .sec-news .sec-row .sec-right > ul.news-tabs.custom-tab").css("top","0");
+                }
+            });
+
+            $(window).resize(function(){
+                var tab_detail_pos = $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").offset().top;
+                if($(window).width()>=768) {
+                    tab_detail_pos = $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").offset().top;
+
+                    $("body.page-news .sec-news .sec-row .sec-right > ul.news-tabs.custom-tab").each(function(){
+                        if(($(window).scrollTop() > tab_detail_pos - 1) && (tab_detail_pos + $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").outerHeight()) > ($(window).scrollTop() + $(this).outerHeight() + 1)) {
+                            side_tab_top = $(window).scrollTop() - tab_detail_pos + 1;
+                            $(this).css('top',side_tab_top + 'px');
+                        } else if ($(window).scrollTop() < tab_detail_pos || $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").outerHeight() < $(this).outerHeight()) {
+                            side_tab_top = 0;
+                            $(this).css('top',side_tab_top + 'px');
+                        } else {
+                            side_tab_top = $("body.page-news .sec-news .sec-row .sec-left > ul.news-list").outerHeight() - $(this).outerHeight();
+                            $(this).css('top',side_tab_top + 'px');
+                        }
+                    });
+                } else {
+                    $("body.page-news .sec-news .sec-row .sec-right > ul.news-tabs.custom-tab").css("top","0");
+                }
+            });
+        }
     });
 });
